70bcf93aec80695d15f8fb2e4aa2631a8c8b0cc7
g.morales <g.morales@samsung.com>
2026-02-02 18:06:43 +0800

    bubble sort refactored as a function

--- algorithms/sorting/bubble_sort.py
+++ algorithms/sorting/bubble_sort.py
@@ -1,9 +1,18 @@
-a = [1,5,4,3]
-last_unsorted_index = len(a) - 1
-for i in range(len(a)):
-    for i in range(last_unsorted_index):
-        if a[i] > a[i+1]:
-    		# swap or bubble the value so it goes to the top
-            a[i+1], a[i] = a[i], a[i+1]
-    last_unsorted_index = last_unsorted_index - 1
-print(a)
+def bubble_sort(arr):
+    last_unsorted_index = len(arr) - 1
+    for i in range(len(a)):
+        for i in range(last_unsorted_index):
+            if a[i] > a[i+1]:
+                # swap or bubble the value so it goes to the top
+                a[i+1], a[i] = a[i], a[i+1]
+        last_unsorted_index = last_unsorted_index - 1
+    return arr
+
+
+if __name__ == "__main__":
+    a = [1,5,4,3]
+    print("Unsorted:")
+    print(a)
+    bubble_sort(a)
+    print("Sorted:")
+    print(a)
